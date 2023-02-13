5b621033e9035bf6b9ca0790c48f07a9e6587864
JungiinChoi <jchoi177@jhu.edu>
2023-02-13 16:12:24 -0500

    add functions

---
+++ R/hiper_glm.R
@@ -0,0 +1,12 @@
+#' @export
+hiper_glm <- function(design, outcome, model = "linear") {
+  supported_model <- c("linear", "logit")
+  if (!(model %in% supported_model)) {
+    stop(sprintf("The model %s is not supported.", model))
+  }
+  warning("`hiper_glm` is yet to be implemented.")
+  # TODO: maximize likelihood
+  hglm_out <- list()
+  class(hglm_out) <- "hglm"
+  return(hglm_out)
+}
