8b594b4f62fccd55c4de7fe4135725b42ed4f2bb
JungiinChoi <jchoi177@jhu.edu>
2023-02-23 02:30:13 -0500

    add analytic functions and complete hiper_glm for linear

---
+++ R/analytic_functions.R
@@ -0,0 +1,18 @@
+#' Compute log-likelihood for linear model
+log_likelihood <- function(design, beta, outcome, noise_var = 1){
+  residual = outcome - design %*% beta
+  return(-crossprod(residual)/(2*noise_var))
+}
+
+#' Compute numeric gradient of log-likelihood for linear model
+numeric_grad <- function(design, beta, outcome, noise_var = 1){
+  residual = outcome - design %*% beta
+  return(-crossprod(design, residual)/noise_var)
+}
+
+#' Optimization via BFGS
+optim_bfgs <- function(init_beta, design, outcome, noise_var = 1){
+  optim_result <- optim(init_beta, log_likelihood, numeric_grad,
+                        design=design, outcome=outcome, method='BFGS')
+  return(optim_result$par)
+}
--- tests/testthat/test-mle-finder.R
+++ tests/testthat/test-mle-finder.R
@@ -1,4 +1,4 @@
-test_that("linalg and optim least-sq coincide", {
+testthat::test_that("linalg and optim least-sq coincide", {
   n_obs <- 32; n_pred <- 4
   data <- simulate_data(n_obs, n_pred, model = 'linear', seed = 1918)
   design <- data$design; outcome <- data$outcome
@@ -6,7 +6,31 @@ test_that("linalg and optim least-sq coincide", {
   via_bfgs_out <- hiper_glm(
     design, outcome, model = 'linear', option = list(mle_solver = 'BFGS')
   )
-  expect_true(are_all_close(
+  testthat::expect_true(are_all_close(
     coef(via_linalg_out), coef(via_bfgs_out), abs_tol = 1e-2, rel_tol = 1e-2
   ))
+})
+
+# Test analytic gradient calculation by comparing it against a numerical one
+testthat::test_that("Numerical and my gradients least-sq coincide", {
+
+  #' Calculate analytic gradient
+  approx_grad <- function(func, x, dx = .Machine$double.eps^(1/3),...) {
+    numerical_grad <- rep(0, length(x))
+    e <- diag(x=dx, nrow=length(x))
+    for (i in 1:length(x)){
+      numerical_grad[i] <- (func(x+e[i,],...)-func(x-e[i,],...))/(2*dx)
+    }
+    return(numerical_grad)
+  }
+
+  n_obs <- 32; n_pred <- 4
+  data <- simulate_data(n_obs, n_pred, model = 'linear', seed = 1918)
+  design <- data$design; outcome <- data$outcome
+  beta <- data$coef_true
+  grad_analytic <-
+    grad_numeric <- approx_grad(log_likelihood, beta,
+                                design = design, outcome = outcome)
+  testthat::expect_true(are_all_close(grad_analytic, grad_numeric,
+                                      abs_tol = 1e-2, rel_tol = 1e-2))
 })
